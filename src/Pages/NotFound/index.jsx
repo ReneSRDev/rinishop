0d828d37b4c109cabf9095b5e71cff9a26dfcb89
ReneSRDev <rene141288@gmail.com>
2023-10-10 22:28:19 -0600

    Fixed errors in the Nav and add style to NotFound Page

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,9 +1,10 @@
 import Layout from '../../Components/Layout';
+import './style.css'
 
 function NotFound() {
     return (
         <Layout>
-            <span>Not Found</span>
+            <span className='index-notfound'>Not Found</span>
         </Layout>
     )
 }
